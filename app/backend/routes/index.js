36395c14c85e08644f850acd44b6872ce9ce7373
Esteban Dalel R <estebandalelr@gmail.com>
2018-02-22 10:32:13 -0500

    Reading from spreadsheet

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+const express = require('express');
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('./client_secret.json');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
+  console.log('Calling');
   res.render('index', { title: 'Express' });
+  getTwits();
+});
+
+/* Helper func to get the tweets */
+function getTwits() {
+  const spreadsheetID = '1pNFVnPjmPkgng2r6BAan85JPElH2TR1sZGZFZdS7rH0';
+  var doc = new GoogleSpreadsheet(spreadsheetID);
+  doc.useServiceAccountAuth(creds, function (err) {
+ 
+    // Get all of the rows from the spreadsheet.
+    doc.getRows(1, function (err, rows) {
+      console.log(rows);
+    });
+  });
+}
+
+
+/* GET tweetArray */
+router.get('/twits', function (req, res) {
+getTwits();
 });
 
 module.exports = router;
