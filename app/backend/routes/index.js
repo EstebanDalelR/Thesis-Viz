db9c3863879f68e1c9c3c0e0febf7db8963ff22a
Esteban Dalel R <estebandalelr@gmail.com>
2018-02-22 13:37:54 -0500

    Got a callback to the first sheet to work

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -6,28 +6,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  console.log('Calling');
   res.render('index', { title: 'Express' });
-  getTwits();
 });
 
-/* Helper func to get the tweets */
-function getTwits() {
+/* Helper func to get the tweeter handle from "camara"*/
+function getCamara() {
   const spreadsheetID = '1pNFVnPjmPkgng2r6BAan85JPElH2TR1sZGZFZdS7rH0';
   var doc = new GoogleSpreadsheet(spreadsheetID);
   doc.useServiceAccountAuth(creds, function (err) {
- 
-    // Get all of the rows from the spreadsheet.
-    doc.getRows(1, function (err, rows) {
-      console.log(rows);
-    });
+    var toRet =
+      doc.getCells(
+        1,
+        { "min-row": 2, "min-col": 1, "max-col": 4 },
+        function (err, cells) {
+          var representantesCamara = [];
+          let cellValues = [];
+          cells.forEach(element => {
+            cellValues.push(element._value);
+          });
+          while (cellValues.length > 0) {
+            var repCamara = [];
+            for (let col = 0; col < 4; col++) {
+              repCamara.push(cellValues.pop());
+            }
+            representantesCamara.push(repCamara);
+          }
+          console.log(representantesCamara);
+          return representantesCamara;
+        });
+    return toRet;
   });
 }
 
 
 /* GET tweetArray */
-router.get('/twits', function (req, res) {
-getTwits();
+router.get('/camara', function (req, res) {
+  getCamara();
+
 });
 
 module.exports = router;
