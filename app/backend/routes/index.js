168fa982983b80cd5262b56ea20a433b19d99da7
Esteban Dalel R <estebandalelr@gmail.com>
2018-02-22 20:06:35 -0500

    Documentation and aesthetic changes

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -4,6 +4,7 @@ const creds = require('./client_secret.json');
 
 var router = express.Router();
 
+/* Const of the spreadsheetID (found in the URL) */
 const spreadsheetID = '1pNFVnPjmPkgng2r6BAan85JPElH2TR1sZGZFZdS7rH0';
 var doc = new GoogleSpreadsheet(spreadsheetID);
 
@@ -13,38 +14,38 @@ router.get('/', function (req, res) {
 });
 
 
-/* Helper func to get the tweeter handle from "camara"*/
+/* Helper func to get the contents from "camara"*/
 function getCamara(callback) {
-  doc.useServiceAccountAuth(
-    creds,
-    (err, resp) => {
-      if (err) throw err;
-      var representantesCamara =
-        doc.getCells(
-          1,
-          { "min-row": 1, "min-col": 1, "max-col": 4 },
-          (err2, cells) => {
-            if (err2) throw err2;
-            var representantesCamara = [];
-            let cellValues = [];
-            cells.forEach(element => {
-              cellValues.push(element._value);
-            });
-            while (cellValues.length > 0) {
-              var repCamara = [];
-              for (let col = 0; col < 4; col++) {
-                repCamara.push(cellValues.pop());
-              }
-              representantesCamara.push(repCamara);
-            }
-            callback(representantesCamara);
-          })
+  /* Authentication method */
+  doc.useServiceAccountAuth(creds, (err, resp) => {
+    if (err) throw err;
+
+    /* Now that we've auth'd, get all the cells */
+    doc.getCells(1, { "min-row": 1, "min-col": 1, "max-col": 4 }, (err2, cells) => {
+      if (err2) throw err2;
+
+      var representantesCamara = [];
+      let cellValues = [];
+
+      /* Get all the values into cellValues */
+      cells.forEach(element => {
+        cellValues.push(element._value);
+      });
+
+      /* While cellValues != empty, push 4 elements (a row) as an object */
+      while (cellValues.length > 0) {
+        var repCamara = [];
+        for (let col = 0; col < 4; col++) {
+          repCamara.push(cellValues.pop());
+        }
+        representantesCamara.push(repCamara);
+      }
+      callback(representantesCamara);
     })
+  })
 }
 
-
-
-/* GET tweetArray */
+/* GET JSON from /camara */
 router.get('/camara', (req, res) =>
   getCamara((representantes) => {
     res.json(representantes);
