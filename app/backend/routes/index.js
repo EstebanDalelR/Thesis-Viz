e3a480cc35e5d23e8076f54680c79b1bd4f3024c
Esteban Dalel R <estebandalelr@gmail.com>
2018-02-23 07:33:14 -0500

    Added routes to hashtags and Medios

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* ---------------------------CAMARA----------------------------- */
 
 /* Helper func to get the contents from "camara"*/
 function getCamara(callback) {
@@ -29,7 +30,7 @@ function getCamara(callback) {
 
       /* Get all the values into cellValues */
       cells.forEach(element => {
-        cellValues.push(element._value);
+        cellValues.unshift(element._value);
       });
 
       /* While cellValues != empty, push 4 elements (a row) as an object */
@@ -49,6 +50,84 @@ function getCamara(callback) {
 router.get('/camara', (req, res) =>
   getCamara((representantes) => {
     res.json(representantes);
+  })
+);
+
+/* ---------------------------MEDIOS----------------------------- */
+
+/* Helper func to get the contents from "medios"*/
+function getMedios(callback) {
+  /* Authentication method */
+  doc.useServiceAccountAuth(creds, (err, resp) => {
+    if (err) throw err;
+
+    /* Now that we've auth'd, get all the cells */
+    doc.getCells(2, { "min-row": 1, "min-col": 1, "max-col": 2 }, (err2, cells) => {
+      if (err2) throw err2;
+
+      var medios = [];
+      let cellValues = [];
+
+      /* Get all the values into cellValues */
+      cells.forEach(element => {
+        cellValues.unshift(element._value);
+      });
+      /* While cellValues != empty, push 4 elements (a row) as an object */
+      while (cellValues.length > 0) {
+        var medio = [];
+        for (let col = 0; col < 2; col++) {
+          medio.push(cellValues.pop());
+        }
+        medios.push(medio);
+      }
+      callback(medios);
+    })
+  })
+}
+
+/* GET JSON from /medios */
+router.get('/medios', (req, res) =>
+  getMedios((medios) => {
+    res.json(medios);
+  })
+);
+
+/* ---------------------------HASHTAGS----------------------------- */
+
+/* Helper func to get the contents from "hashtags"*/
+function getHashtags(callback) {
+  /* Authentication method */
+  doc.useServiceAccountAuth(creds, (err, resp) => {
+    if (err) throw err;
+
+    /* Now that we've auth'd, get all the cells */
+    doc.getCells(5, { "min-row": 1, "min-col": 1 }, (err2, cells) => {
+      if (err2) throw err2;
+
+      var hashtags = [];
+      let cellValues = [];
+
+      /* Get all the values into cellValues */
+      cells.forEach(element => {
+        cellValues.unshift(element._value);
+      });
+      /* While cellValues != empty, push 4 elements (a row) as an object */
+      while (cellValues.length > 0) {
+        var hashtag = [];
+        for (let col = 0; col < 1; col++) {
+          hashtag.push(cellValues.pop());
+        }
+        hashtags.push(hashtag);
+      }
+      callback(hashtags);
+    })
+  })
+}
+
+/* GET JSON from /hashtags */
+router.get('/hashtags', (req, res) =>
+  getHashtags((hashtags) => {
+    res.json(hashtags);
   })
 );
 
