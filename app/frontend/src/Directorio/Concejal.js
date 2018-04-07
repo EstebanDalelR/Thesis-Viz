1955c04b6a089df70d425d45f1df3f7f9e935480
Esteban Dalel R <estebandalelr@gmail.com>
2018-04-07 18:23:22 -0500

    Modified Concejal to fit mockup, changed CSS file name

--- app/frontend/src/Directorio/Concejal.js
+++ app/frontend/src/Directorio/Concejal.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import '../CSS/BioConcejal.css';
-import './bogota.jpg';
+import '../CSS/Concejal.css';
 class Concejal extends Component {
     constructor(props) {
         super(props);
@@ -8,6 +7,7 @@ class Concejal extends Component {
             "concejal": this.props.info,
         }
     }
+
     render() {
         return (
             <div className="bioConcejal">
@@ -15,10 +15,9 @@ class Concejal extends Component {
                     className="fotoPersona"
                     src={this.state.concejal.foto}
                     alt={"Foto de " + this.state.concejal.nombre} />
-                <h1 className="nombrePersona">H.C. {this.state.concejal.nombre}</h1>
+                <h1 className="nombrePersona">{this.state.concejal.nombre}</h1>
                 <div className="periodosAnteriores">
                     <p>Comision {this.state.concejal.comision}</p>
-
                 </div>
                 <div className="mapa">
                     <img alt="mapa de Bogota" src='http://cdn.radiosantafe.com//wp-content/uploads/2016/01/BOGOTA-MAPA.jpg' />
@@ -31,11 +30,30 @@ class Concejal extends Component {
                     <h3>Otros de {this.state.concejal.partido}</h3>
 
                 </div>
+                <div className="otrosDeComision">
+                    <h3>Otros de {this.state.concejal.comision}</h3>
+                </div>
                 <div className="twitterFeed">
-                    <p>Twitts de @{this.state.concejal.twitter}</p>
+                    <p>Tweets de @{this.state.concejal.twitter}</p>
                 </div>
                 <div className="hojaDeVida">
                     <h3>Hoja de Vida</h3>
+                    <hr />
+                </div>
+                <div className="proyectosAcuerdo">
+                    <h3>Proyectos de Acuerdo</h3>
+                    <hr />
+                </div>
+                <div className="mesasTrabajo">
+                    <h3>Mesas de Trabajo</h3>
+                    <hr />
+                </div>
+                <div className="asistencia">
+                    <h3>Asistencia y Participación</h3>
+                    <hr />
+                </div>
+                <div className="controlPolitico">
+                    <h3>Citaciones a Control Político</h3>
                     <hr />
                 </div>
             </div>
