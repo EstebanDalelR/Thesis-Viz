b1d310ae19dd729cf1c5d89596f4874e3752c82e
Esteban Dalel R <estebandalelr@gmail.com>
2018-05-13 14:11:54 -0500

    Update map with colors, remove unused code

--- app/frontend/src/Directorio/Mapa.js
+++ app/frontend/src/Directorio/Mapa.js
@@ -25,14 +25,12 @@ class Mapa extends Component {
         });
         return votLoc;
     }
-    componentDidMount(){
+
+    componentDidMount() {
         var margin = { top: 20, bottom: 20, right: 20, left: 20 };
         var width = "800";
         var height = "800";
         var svg = d3.select("svg");
-        var color = d3.scaleThreshold()
-            .domain(d3.range(0, 100000))
-            .range(d3.schemeBlues[9]);
 
         let votLoc = this.matcher();
 
@@ -55,32 +53,69 @@ class Mapa extends Component {
                 "fill",
                 (d) => {
                     let nomLoc = d.properties.name;
-                    let value;
+                    let value = "rgb(255,255,255)";
                     votLoc.forEach(element => {
                         if (element[0] === nomLoc) {
-                            if (parseInt(element[1],10) === 0) {
-                                value = "rgb(255,255,255)";
-                            } else if (parseInt(element[1],10) <= 500) {
-                                value = "rgb(209, 226, 255)";
-                            } else if (parseInt(element[1],10) <= 1000) {
-                                value = "rgb(155, 192, 255)";
-                            } else if (parseInt(element[1],10) <= 1500) {
-                                value = "rgb(107, 161, 255)";
-                            } else if (parseInt(element[1],10) <= 2000) {
-                                value = "rgb(63, 133, 255))";
-                            } else if (parseInt(element[1],10) <= 3000) {
-                                value = "rgb(53, 103, 255))";
-                            } else if (parseInt(element[1],10) <= 5000) {
-                                value = "rgb(33, 93, 255))";
-                            } else if (parseInt(element[1],10) <= 8000) {
-                                value = "rgb(23, 73, 255))";
-                            } else if (parseInt(element[1],10) <= 10000) {
-                                value = "rgb(13, 53, 255))";
-                            } else if (parseInt(element[1],10) <= 20000) {
-                                value = "rgb(13, 33, 255))";
-                            }
-                            else {
-                                value = "rgb(0, 0, 255))";
+                            if (parseInt(element[1], 10) === 0) {
+                                return value;
+                            } else if (parseInt(element[1], 10) <= 25) {
+                                return value = 'rgb(244, 244, 255)';
+                            } else if (parseInt(element[1], 10) <= 50) {
+                                return value = 'rgb(233, 233, 254)';
+                            } else if (parseInt(element[1], 10) <= 75) {
+                                return value = 'rgb(222, 222, 254)';
+                            } else if (parseInt(element[1], 10) <= 100) {
+                                return value = 'rgb(211, 211, 253)';
+                            } else if (parseInt(element[1], 10) <= 200) {
+                                return value = 'rgb(200,200,253)';
+                            } else if (parseInt(element[1], 10) <= 300) {
+                                return value = 'rgb(189,189,252)';
+                            } else if (parseInt(element[1], 10) <= 400) {
+                                return value = 'rgb(178,178,252)';
+                            } else if (parseInt(element[1], 10) <= 500) {
+                                return value = 'rgb(167,167,251)';
+                            } else if (parseInt(element[1], 10) <= 600) {
+                                return value = 'rgb(156,156,251)';
+                            } else if (parseInt(element[1], 10) <= 700) {
+                                return value = 'rgb(145,145,250)';
+                            } else if (parseInt(element[1], 10) <= 800) {
+                                return value = 'rgb(134,134,250)';
+                            } else if (parseInt(element[1], 10) <= 900) {
+                                return value = 'rgb(123,123,249)';
+                            } else if (parseInt(element[1], 10) <= 1000) {
+                                return value = 'rgb(112,112,249)';
+                            } else if (parseInt(element[1], 10) <= 2000) {
+                                return value = 'rgb(101,101,248)';
+                            } else if (parseInt(element[1], 10) <= 3000) {
+                                return value = 'rgb(90,90,248)';
+                            } else if (parseInt(element[1], 10) <= 4000) {
+                                return value = 'rgb(79,79,247)';
+                            } else if (parseInt(element[1], 10) <= 5000) {
+                                return value = 'rgb(68,68,247)';
+                            } else if (parseInt(element[1], 10) <= 6000) {
+                                return value = 'rgb(39,39,246)';
+                            } else if (parseInt(element[1], 10) <= 7000) {
+                                return value = 'rgb(57,57,246)';
+                            } else if (parseInt(element[1], 10) <= 8000) {
+                                return value = 'rgb(51,51,246)';
+                            } else if (parseInt(element[1], 10) <= 9000) {
+                                return value = 'rgb(49,49,246)';
+                            } else if (parseInt(element[1], 10) <= 10000) {
+                                return value = 'rgb(46,46,246)';
+                            } else if (parseInt(element[1], 10) <= 15000) {
+                                return value = 'rgb(39,39,246)';
+                            } else if (parseInt(element[1], 10) <= 20000) {
+                                return value = 'rgb(35,35,246)';
+                            } else if (parseInt(element[1], 10) <= 25000) {
+                                return value = 'rgb(29,29,246)';
+                            } else if (parseInt(element[1], 10) <= 30000) {
+                                return value = 'rgb(24,24,246)';
+                            } else if (parseInt(element[1], 10) <= 35000) {
+                                return value = 'rgb(19,19,246)';
+                            } else if (parseInt(element[1], 10) <= 40000) {
+                                return value = 'rgb(13,13,246)';
+                            } else if (parseInt(element[1], 10) <= 50000) {
+                                return value = 'rgb(2,2,246)';
                             }
                         }
                     })
@@ -96,7 +131,7 @@ class Mapa extends Component {
                     let nomLoc = d.properties.name;
                     votLoc.forEach(element => {
                         if (element[0] === nomLoc) {
-                            text = nomLoc + ":" + element[1]
+                            text = nomLoc + ": " + element[1]
                         }
                     })
                     return text;
@@ -111,7 +146,7 @@ class Mapa extends Component {
             .attr("d", path);
     }
     render() {
-        
+
         return (
             <svg viewBox="0 0 800 800" width="100%" height="100%">
 
